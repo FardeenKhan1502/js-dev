b04669dfbdb431568c9a3883e0e81332a6d64bd8
Fardeen Khan <fkhangaria@gmail.com>
2025-09-17 02:17:31 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accoutId=14453
+let accoutEmail='fkhangaria@gmail.com'
+var accountPassword="12345"
+accountCity="Jaipur"
+//accoutId=2
+console.log(accoutId)
+console.table({accountPassword,accoutEmail,accoutId,accountCity}
+);
+
